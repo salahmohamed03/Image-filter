7d59abb08afd493ae63aa79e594560556a4b4223
salahmohamed <salahmohamed.sm2016@gmail.com>
2025-02-21 17:28:23 +0200

    upload and display images

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from gui import PubSubGUI
+
+from MainWindowUI import MainWindowUI
 from fourier import Fourier
 import asyncio
-import qasync
-import Filters
 from qasync import QEventLoop
 
 async def main():
     # Initialize classes
     fourier = Fourier()
-    filters = Filters.Filters()
+
     ###
     
     app = QApplication(sys.argv)
     loop = QEventLoop(app)
     asyncio.set_event_loop(loop)
     
-
-    window = PubSubGUI()
+    window = MainWindowUI()
     window.show()
     with loop:
         loop.run_forever()
